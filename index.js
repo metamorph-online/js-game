2755ad15dafe5c60f10666221c0988934dc808ad
metamorph-online <metamorph_online@yahoo.com>
2019-06-23 00:33:41 +0300

    Ping-pong page start

---
+++ index.js
@@ -0,0 +1,40 @@
+import "./styles.css";
+import Paddle from './paddle';
+import InputHandler from './input';
+
+let canvas = document.getElementById('gameScreen');
+let ctx = canvas.getContext("2d");
+	
+
+
+const GAME_WIDTH =  800;
+const GAME_HEIGHT = 600;
+
+let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
+
+new InputHandler(paddle);
+
+paddle.draw(ctx);
+
+let lastTime = 0;
+
+let imgBall = document.getElementById('img_ball');
+
+function gameLoop(timestamp){
+
+	let deltaTime = timestamp - lastTime;
+
+	lastTime = timestamp
+
+	ctx.clearRect(0, 0, 800, 600);
+
+	paddle.update(deltaTime);
+
+	paddle.draw(ctx);
+
+	ctx.drawImage(imgBall, 10, 10);
+
+	requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
